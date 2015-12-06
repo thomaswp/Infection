91c05e0201916441ee7a7811f561f92758dd62e4
thomaswp <thomaswprice@msn.com>
2015-12-06 08:50:47 -0500

    Done, just need tests

---
+++ src/org/khanacademy/infection/Population.java
@@ -0,0 +1,112 @@
+package org.khanacademy.infection;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Population {
+
+	private int nextUserID = 0;
+	private Set<User> allUsers = new HashSet<>();
+	
+	protected int incrementUserID() {
+		return nextUserID++;
+	}
+	
+	public User createUser(String userName) {
+		User user = new User(userName, this);
+		allUsers.add(user);
+		return user;
+	}
+	
+	public boolean removeUser(User user) {
+		return allUsers.remove(user);
+	}
+	
+	public Set<Infection> getInfections() {
+		Set<Infection> infections = new HashSet<>();
+		for (User user : allUsers) infections.add(user.getInfection());
+		return infections;
+	}
+	
+	/**
+	 * Calls {@link Population#limitedInfection(String, int, int)} with 
+	 * threshold = 0.
+	 */
+	public int limitedInfection(String condition, int n) {
+		return limitedInfection(condition, n, 0);
+	}
+	
+	/**
+	 * Infects approximately n users with the given condition. 
+	 * The algorithm will avoid breaking up any infection groups if it is possible
+	 * to do so while still infecting at least (n - threshold) users.
+	 * If this is not possible, the algorithm will infect as many whole infection
+	 * groups as possible, and then infect only a portion of one additional infection
+	 * group such that n total users are infected.
+	 * The only circumstance under which fewer than (n - threshold) users will be infected
+	 * is if the total users is fewer than n.
+	 * 
+	 * @param condition The condition with which to infect users
+	 * @param n The desired number of users to infect
+	 * @param threshold The threshold of users less than n which can be infected
+	 * if this will allow all infection groups to maintain the same condition.
+	 * @return The total number of users infected.
+	 */
+	public int limitedInfection(String condition, int n, int threshold) {
+		
+		Set<Infection> infections = getInfections();
+
+		int infected = 0;
+		// We use an approximating version of SubsetSum here, rather than the exact one,
+		// because we want the closest possible sum, which means our "threshold"
+		// for SubsetSum is infinity, which breaks the dynamic programming solution
+		List<Infection> subset = SubsetSum.subsetSumApproximate(infections, n - infected);
+		
+		while (subset.size() >= 0) {
+			infections.removeAll(subset);
+			for (Infection infection : subset) {
+				infection.addCondition(condition);
+				infected += infection.size();
+			}
+			subset = SubsetSum.subsetSumApproximate(infections, n - infected);
+		}
+		
+		if (infections.size() == 0 || Math.abs(infected - n) <= threshold) {
+			return infected;
+		}
+		
+		List<Infection> list = new ArrayList<Infection>(infections);
+		Collections.sort(list, SubsetSum.COUNTABLE_COMPARATOR);
+		Infection largest = list.get(0);
+		
+		largest.infectUpTo(condition, n - infected);
+		return n;
+	}
+	
+	/**
+	 * If threshold if 0, infects exactly n Users with the given condition without 
+	 * breaking up any infection groups, or fails if this is not possible.
+	 * If threshold is not 0, the exact number of infected users can be
+	 * be m, where (n - threshold <= m <= n + threshold).
+	 * We return the actual number of users infected, or -1 for failure.
+	 * @param condition The condition with which to infect users
+	 * @param n The number of users to infect
+	 * @param threshold A margin of error n.
+	 * @return The exact number of users infected, or -1 for failure
+	 */
+	public int limitedInfectionExact(String condition, int n, int threshold) {
+		Set<Infection> infections = getInfections();
+		List<Infection> subset = SubsetSum.subsetSum(infections, n, 0);
+		if (subset == null) return -1;
+		int infected = 0;
+		for (Infection infection : subset) {
+			infected += infection.size();
+			infection.addCondition(condition);
+		}
+		return infected;
+	}
+	
+}
--- src/org/khanacademy/infection/User.java
+++ src/org/khanacademy/infection/User.java
@@ -1,19 +1,13 @@
 package org.khanacademy.infection;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 public class User {
-
-	private static int nextUserID = 0;
-	private static Set<User> allUsers = new HashSet<>();
 	
 	private String userName;
 	private int subgraphSize = 1;
-	private int userID = nextUserID++;
+	private int userID;
 	
 	private Infection infection;
 	
@@ -23,6 +17,8 @@ public class User {
 	private final Set<User> pupils = new HashSet<>();
 	
 	private final HashSet<String> conditions = new HashSet<>();
+	
+	private final Population population;
 	
 	public String getUserName() {
 		return userName;
@@ -48,15 +44,12 @@ public class User {
 		this.infection = infection;
 	}
 	
-	public User() {
-		this("Anonymous");
-	}
-	
-	public User(String userName) {
+	protected User(String userName, Population population) {
 		this.userName = userName;
+		this.population = population;
 		infection = new Infection();
 		infection.addUser(this);
-		allUsers.add(this);
+		userID = population.incrementUserID();
 	}
 	
 	public static boolean addCoach(User coach, User pupil) {
@@ -74,7 +67,7 @@ public class User {
 		infection.removeUser(this);
 		for (User pupil : pupils) pupil.coaches.remove(this);
 		for (User coach : coaches) coach.pupils.remove(this);
-		allUsers.remove(this);
+		population.removeUser(this);
 	}
 	
 	public static boolean removeCoach(User coach, User pupil) {
@@ -107,78 +100,7 @@ public class User {
 		return conditions.contains(condition);
 	}
 	
-	public static void infect(User user, String condition) {
-		user.infection.addCondition(condition);
-	}
-
-	private static Set<Infection> getInfections() {
-		Set<Infection> infections = new HashSet<>();
-		for (User user : allUsers) infections.add(user.infection);
-		return infections;
-	}
-	
-	public static int limitedInfection(String condition, int n) {
-		return limitedInfection(condition, n, 0);
-	}
-	
-	/**
-	 * Infects approximately n users with the given condition. 
-	 * The algorithm will avoid breaking up any infection groups if it is possible
-	 * to do so while still infecting at least (n - threshold) users.
-	 * If this is not possible, the algorithm will infect as many whole infection
-	 * groups as possible, and then infect only a portion of one additional infection
-	 * group such that n total users are infected.
-	 * The only circumstance under which fewer than (n - threshold) users will be infected
-	 * is if the total users is fewer than n.
-	 * 
-	 * @param condition The condition with which to infect users
-	 * @param n The desired number of users to infect
-	 * @param threshold The threshold of users less than n which can be infected
-	 * if this will allow all infection groups to maintain the same condition.
-	 * @return The total number of users infected.
-	 */
-	public static int limitedInfection(String condition, int n, int threshold) {
-		
-		Set<Infection> infections = getInfections();
-
-		int infected = 0;
-		List<Infection> subset = SubsetSum.subsetSumApproximate(infections, n - infected);
-		
-		while (subset.size() >= 0) {
-			infections.removeAll(subset);
-			for (Infection infection : subset) {
-				infection.addCondition(condition);
-				infected += infection.size();
-			}
-			subset = SubsetSum.subsetSumApproximate(infections, n - infected);
-		}
-		
-		if (infections.size() == 0 || Math.abs(infected - n) <= threshold) {
-			return infected;
-		}
-		
-		List<Infection> list = new ArrayList<Infection>(infections);
-		Collections.sort(list, SubsetSum.COUNTABLE_COMPARATOR);
-		Infection largest = list.get(0);
-		
-		largest.infectUpTo(condition, n - infected);
-		return n;
-	}
-	
-	/**
-	 * Infects exactly n Users with the given condition without breaking up
-	 * and infection groups, or return false if this is not possible.
-	 * @param condition The condition with which to infect users
-	 * @param n The number of users to infect
-	 * @return Whether or not the operation succeeded.
-	 */
-	public static boolean limitedInfectionExact(String condition, int n) {
-		Set<Infection> infections = getInfections();
-		List<Infection> subset = SubsetSum.subsetSum(infections, n, 0);
-		if (subset == null) return false;
-		for (Infection infection : subset) {
-			infection.addCondition(condition);
-		}
-		return true;
+	public void infect(String condition) {
+		infection.addCondition(condition);
 	}
 }
